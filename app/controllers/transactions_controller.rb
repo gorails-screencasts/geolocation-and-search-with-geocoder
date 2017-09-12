7f33feea8e93cc3fb4acefeea5249ca9b96410d5
Chris Oliver <excid3@gmail.com>
2017-09-12 15:54:48 -0500

    Add geo search

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,7 +4,18 @@ class TransactionsController < ApplicationController
   # GET /transactions
   # GET /transactions.json
   def index
-    @transactions = Transaction.page(params[:page]).per(5)
+    @transactions = if params[:l]
+                      sw_lat, sw_lng, ne_lat, ne_lng = params[:l].split(",")
+                      center   = Geocoder::Calculations.geographic_center([[sw_lat, sw_lng], [ne_lat, ne_lng]])
+                      distance = Geocoder::Calculations.distance_between(center, [sw_lat, sw_lng])
+                      box      = Geocoder::Calculations.bounding_box(center, distance)
+                      Transaction.within_bounding_box(box)
+                    elsif params[:near]
+                      Transaction.near(params[:near])
+                    else
+                      Transaction.all
+                    end
+    @transactions = @transactions.page(params[:page]).per(5)
   end
 
   # GET /transactions/1
--- app/javascript/packs/maps/index.js
+++ app/javascript/packs/maps/index.js
@@ -1,5 +1,4 @@
 document.addEventListener("turbolinks:load", function() {
-
   var map = new GMaps({
     div: '#map',
     lat: 38.5816,
@@ -23,5 +22,23 @@ document.addEventListener("turbolinks:load", function() {
     }
   });
 
-  map.fitZoom();
+  var l = document.querySelector("#map").dataset.l;
+  if (l) {
+    var latlngs   = l.split(',');
+    var southWest = new google.maps.LatLng(latlngs[0], latlngs[1]);
+    var northEast = new google.maps.LatLng(latlngs[2], latlngs[3]);
+    var bounds    = new google.maps.LatLngBounds(southWest, northEast);
+    map.fitBounds(bounds, 0);
+  } else {
+    map.fitZoom();
+  }
+
+  document.querySelector("#redo-search").addEventListener("click", function(e) {
+    e.preventDefault();
+
+    var bounds = map.getBounds();
+    var location = bounds.getSouthWest().toUrlValue() + "," + bounds.getNorthEast().toUrlValue();
+
+    Turbolinks.visit(`/transactions?l=${location}`);
+  });
 });
